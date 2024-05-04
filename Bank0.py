20bf605a95a141352372454569348fde42782009
SophiaNCar <sophiancarballo@gmail.com>
2024-05-04 13:42:52 -0500

    bank

---
+++ Bank0.py
@@ -0,0 +1,14 @@
+#I think my og bank software might have been deleted, so let me speedrun
+import _tkinter
+
+#this is the welcome message
+def welcome():
+    print("Hello. Welcome to [Bank Name]!\n We're happy to help you.")
+    print("Please obey the law, or consequences will come, so act accordingly.")
+
+welcome()
+def userType():
+    input("First thing's first, are you a returning user or a new user?")
+
+    def newUser():
+        
