8d3c8a3a5eea7b4c8772a93cef1a11916f8c7671
SophiaNCar <sophiancarballo@gmail.com>
2024-05-05 09:19:43 -0500

    bbbbb yeahhhhhhhhhh

--- Bank0.py
+++ Bank0.py
@@ -7,8 +7,20 @@ def welcome():
     print("Please obey the law, or consequences will come, so act accordingly.")
 
 welcome()
+
+#asks the user if they have used this bank before or not and determines what to do depending on their
+#answer
 def userType():
     input("First thing's first, are you a returning user or a new user?")
 
-    def newUser():
-        
+#The protocol for new users
+def newUser():
+    print("We're happy to have you join us.")
+    username = input("Please enter your name.\n")
+    print("Welcome, {username}! Please wait while we generate your ID...")
+
+#the protocol for existing users
+def oldUser():
+    print("Welcome back.")
+    iID = int(input("Please input your user ID.\n"))
+    iPin = int(input("Please input your pin.\n"))
